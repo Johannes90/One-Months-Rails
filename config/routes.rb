569f909f3248583e8b6f24f674bbf55ad2ecb851
Johannes Herrmann <j_herrmann@hotmail.com>
2015-02-28 12:33:51 +0100

    Added Pins Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  resources :pins
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   devise_for :users do
